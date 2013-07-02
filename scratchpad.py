ba971ff56e37186a6405340b29d2d1e1895c618b
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-07-02 18:39:00 +0000

    Scratchpad prints out journey IDs and timestamps

--- scratchpad.py
+++ scratchpad.py
@@ -4,3 +4,10 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('pretty-xml-sample.xml')
 root = tree.getroot()
+
+vehicle_journeys = root[6]
+
+for journey in vehicle_journeys:
+	print journey[3].text # journey ID
+	print journey[7].text # timestamp
+	print ''
